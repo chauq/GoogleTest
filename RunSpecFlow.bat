a7005ca0c2f545c031dec1effa44908741711b61
Quang chau <quang.chau@durabledigital.com>
2015-11-13 16:45:34 +0000

    fixed runspecflow.bat by adding in nunit and changing pathing in .bat file

--- RunSpecFlow.bat
+++ RunSpecFlow.bat
@@ -1,10 +1,10 @@
-"C:\Program Files (x86)\NUnit 2.6.4\bin\nunit-console.exe" /labels /out=TestResult.txt /xml=TestResult.xml /framework=net-4.0 F:\git\GoogleTest\GoogleTest\GoogleTest\bin\Debug\GoogleTest.dll
+GoogleTest\bin\Debug\nunit-console.exe /labels /out=TestResult.txt /xml=TestResult.xml /framework=net-4.0 GoogleTest\bin\Debug\GoogleTest.dll
  
-"F:\git\GoogleTest\GoogleTest\packages\SpecFlow.1.9.0\tools\SpecFlow.exe" nunitexecutionreport F:\git\GoogleTest\GoogleTest\GoogleTest\GoogleTest.csproj
+packages\SpecFlow.1.9.0\tools\SpecFlow.exe nunitexecutionreport F:\git\GoogleTest\GoogleTest\GoogleTest\GoogleTest.csproj
  
 IF NOT EXIST TestResult.xml GOTO FAIL
 IF NOT EXIST TestResult.html GOTO FAIL
-pause
+EXIT
  
 :FAIL
 echo ##teamcity[buildStatus status='FAILURE']
