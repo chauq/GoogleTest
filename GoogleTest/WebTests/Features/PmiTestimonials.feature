0a05df38f306c5499d8b9abdb5a008950c6ae585
Quang chau <quang.chau@durabledigital.com>
2016-01-28 17:38:21 +0000

    Revert "v12.suo issues"
    This reverts commit 27e5357afc7b35e6ed3fab0e8b123296a6fda37a.

---
+++ GoogleTest/WebTests/Features/PmiTestimonials.feature
@@ -0,0 +1,11 @@
+﻿Feature: Pmi Testimonials
+
+Scenario Outline: As a user, If I go to the Testimonials page, I should see some widgets
+	Given I am on the Pmi home page
+	When I go to the testimonials page
+	Then I should see a page with following <widgets>
+	Examples: 
+	| widgets						|
+	| quotation-central				|
+	| quotation-central				|
+	| quotation						|
---
+++ GoogleTest/WebTests/Pages/TestimonialsPage.cs
@@ -0,0 +1,27 @@
+﻿using GoogleTest.WebTests.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace GoogleTest.WebTests.Pages
+{
+    public class TestimonialsPage : BasePage
+    {
+        public TestimonialsPage(IWebDriver driver)
+            : base(driver, typeof(TestimonialsPage).Name)
+        {
+            _link = new Uri(SystemUnderTest.SiteUri + "/" + SystemUnderTest.TestimonialsLink);
+        }
+
+        private Uri _link;
+
+        internal void NavigateHere()
+        {
+            Driver.Navigate().GoToUrl(_link);
+        }
+
+      }
+}
